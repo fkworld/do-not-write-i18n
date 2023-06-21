0dc24118a8e46696dae8d8f038f50e2034d8db80
fy <yong.feng5@lotuscars.com.cn>
2023-06-21 13:53:29 +0800

    feat: add vite-tsconfig-paths for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     environment: "jsdom",
   },
