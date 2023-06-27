4ad2e647936995b00e47866f4a0346783583a189
fkworld <fkworld@foxmail.com>
2023-06-27 13:15:26 +0800

    chore: rename config file from .cjs to .json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
-// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
